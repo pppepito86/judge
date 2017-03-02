eb77193d2821d9468a8309767dcb95c1f0049038
Marin Shalamanov <marin.shalamanov@sap.com>
2017-03-02 23:57:27 +0200

    SubmissionsResource

--- src/main/java/org/pesho/judge/rest/SubmissionsResource.java
+++ src/main/java/org/pesho/judge/rest/SubmissionsResource.java
@@ -2,6 +2,10 @@ package org.pesho.judge.rest;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,30 +14,37 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.pesho.judge.model.Problem;
 import org.pesho.judge.model.Submission;
 
+@Stateless
 @Path("submissions")
 public class SubmissionsResource {
-
+	
+	@PersistenceContext(unitName = "judge")
+	EntityManager em;
+	
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Problem> listSubmisssions() {
-        return null;
+    public List<Submission> listSubmisssions() {
+    	TypedQuery<Submission> query = em.createNamedQuery("Submission.findAll", Submission.class);
+    	List<Submission> results = query.getResultList();
+    	return results;
     }
     
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Submission createSubmission(Submission submission) {
-        return submission;
+    	// TODO
+        return null;
     }
     
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Submission getSubmission(@PathParam("id") int submissionId) {
-        return null;
+    	Submission submission = em.find(Submission.class, (Integer)submissionId);
+        return submission;
     }
     
 }
