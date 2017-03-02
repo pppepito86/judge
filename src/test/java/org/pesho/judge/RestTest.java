e06d5f20b221fb3d8d0ccb11d1dce92e8400cf6a
Petar Petrov <pppepito86@gmail.com>
2017-03-02 19:10:13 +0200

    integration tests skeleton

--- src/test/java/org/pesho/judge/RestTest.java
+++ src/test/java/org/pesho/judge/RestTest.java
@@ -1,14 +1,7 @@
 package org.pesho.judge;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.pesho.judge.model.User;
 
 public class RestTest {
 
@@ -18,13 +11,13 @@ public class RestTest {
 	
 	@Test
 	public void test() {
-		Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("http://localhost:8080/judge.web/rest/");
-    	//Response response = target.path("test").request(MediaType.APPLICATION_JSON).post(Entity.json(role));
-        Response response = target.path("test").request(MediaType.APPLICATION_JSON).get();
-    	User responseRole = (User) response.readEntity(User.class);
-
-        System.out.println(response.getStatus());
-        System.out.println(responseRole);
+//		Client client = ClientBuilder.newClient();
+//        WebTarget target = client.target("http://localhost:8080/judge.web/rest/");
+//    	//Response response = target.path("test").request(MediaType.APPLICATION_JSON).post(Entity.json(role));
+//        Response response = target.path("test").request(MediaType.APPLICATION_JSON).get();
+//    	User responseRole = (User) response.readEntity(User.class);
+//
+//        System.out.println(response.getStatus());
+//        System.out.println(responseRole);
 	}
 }
